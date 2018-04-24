3119d26dd1a5eb033e5bee8d8d7024a9e7903139
Ami Cooper and Garnett Puett <garnett@>
2018-04-24 13:45:13 -0700

    Add XML promise.

--- src/main.js
+++ src/main.js
@@ -8,24 +8,27 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let zip = $('#location').val();
     $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${process.env.WEB_KEY}`;
-
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        getElements(response);
+    let promise;
+    promise = new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${process.env.WEB_KEY}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
       }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    let getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${zip} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    };
+      request.open("GET", url, true);
+      request.send();
+    });
+    let body;
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${zip} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 });
